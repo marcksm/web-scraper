7c73df5d5f533e842b12711d1207732db3e33b75
Marcos Vinicius do Carmo Sousa <marcos.vinicius.wz@gmail.com>
2018-01-05 12:12:03 -0200

    adding data types

--- computer.py
+++ computer.py
@@ -1,49 +1,28 @@
+import re
+
 class Computer:
-    def __init__(self, service, name, priceHr, priceMo, cpus, memRam, ramtype, memSSD, ssdtype, bandwidth):
+    def __init__(self, service, name, priceHr, priceMo, cpus, memRam, memSSD, bandwidth):
         self.name = name
-        self.priceHr = priceHr
-        self.priceMo = priceMo
-        self.cpus = cpus
-        self.memRam = memRam
-        self.ramtype = ramtype
-        self.memSSD = memSSD
-        self.ssdtype = ssdtype
-        self.bandwidth = bandwidth
-#re.findall("\d+\.\d+",
-    def setName(self, name):
-        self.name = name
-    def getName(self):
-        return self.name
-
-    def setpriceHr(self, priceHr):
-        self.priceHr = priceHr
-    def getpriceHr(self):
-        return self.priceHr
-
-    def setpriceMo(self, name):
-        self.priceMo = priceMo
-    def getpriceMo(self):
-        return self.priceMo
-
-    def setcpus(self, cpus):
-        self.cpus = cpus
-    def getcpus(self):
-        return self.cpus
-
-    def setmemRam(self, memRam):
-        self.memRam = memRam
-    def getmemRam(self):
-        return self.memRam
-
-    def setmemSSD(self, memSSD):
-        self.name = memSSD
-    def getmemSSD(self):
-        return self.memSSD
-
-    def setbandwidth(self, bandwidth):
-        self.bandwidth = bandwidth
-    def getbandwidth(self):
-        return self.bandwidth
+        self.service = service
+        self.priceHr = re.findall("\d+\.\d+", priceHr)[0]
+        self.priceMo = int(re.findall("\d+", priceMo)[0])
+        self.cpus = int(re.findall("\d+", cpus)[0])
+        self.memRam = int(re.findall("\d+", memRam)[0])
+        self.ramtype = re.findall("[a-zA-Z]+", memRam)[0]
+        self.memSSD = int(re.findall("\d+", memSSD)[0])
+        self.ssdtype = re.findall("[a-zA-Z]+", memSSD)[0]
+        self.bandwidth = int(re.findall("\d+", bandwidth)[0])
+        self.bandwidthtype = re.findall("[a-zA-Z]+", bandwidth)[0]
+    # 
+    # def convertTo(storageType):
+    #     if !ramtype == storageType:
+    #         if ramtype == 'MB':
+    #             (FORMULA)
+    #         if ramtype == 'TB':
+    #             (FORMULA)
+    #         if ramtype == 'GB':
+    #             (FORMULA)
+    #         ramtype = storageType
 
     def showData(self):
         print ("Name = ", self.name)
@@ -51,5 +30,7 @@ class Computer:
         print ("Price mo = ", self.priceMo)
         print ("CPUS = ", self.cpus)
         print ("RAM = ", self.memRam)
+        print ("RAM = ", self.ramtype)
         print ("SSD = ", self.memSSD)
         print ("Transfer = ", self.bandwidth)
+        print ("Transfer = ", self.bandwidthtype)
--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 import requests
 from lxml import html
 from computer import Computer
-import re
 
 def makeRequests():
     r1 = requests.get('https://www.digitalocean.com/pricing/')
@@ -9,24 +8,27 @@ def makeRequests():
 #    r3 = requests.get('https://www.packet.net/bare-metal/')
     tree = html.fromstring(r1.text)
     computers = tree.xpath('//div[@class="PriceBlock-container"]')
-    #print (computers[0].text_content())
     return computers
 
 def buildComputer(comp_tree):
-    
+
     price_mo = comp_tree.xpath('div/div/div/span[@class="PriceBlock-num"]')[0].text_content()
     price_hr = comp_tree.xpath('div/div/div[@class="u-flex u-flexCenter PriceBlock--secondary"]')[0].text_content()
     mem_ram = comp_tree.xpath('div/ul/li/span')[0].text_content()
     cpus = comp_tree.xpath('div/ul/li')[1].text_content()
     mem_ssd = comp_tree.xpath('div/ul/li')[2].text_content()
     bandwidth = comp_tree.xpath('div/ul/li')[3].text_content()
-    #computer = Computer(service, name, price_hr, price_mo, cpus, mem_ram, mem_ssd, bandwidth)
-    print (bandwidth)
-#    return computer
+    computer = Computer('service', 'name', price_hr, price_mo, cpus, mem_ram, mem_ssd, bandwidth)
+    return computer
 
 def computers():
     comp = makeRequests()
-    computer = buildComputer(comp[0])
-#    computer.showData()
+    #print(len(comp))
+    for i in range (0, len(comp)):
+        computer = buildComputer(comp[i])
+        computer.showData()
+        #sqldata=computer.getData()
+        #save to sqllite
+        print('\n')
 
 computers()
